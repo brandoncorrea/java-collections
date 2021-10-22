3264ec870c836501f6eceb5972a8884bcb335a5b
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-10-22 18:59:08 -0400

    Refactor LinkedList

--- src/collections/linkedList/LinkedList.java
+++ src/collections/linkedList/LinkedList.java
@@ -1,5 +1,9 @@
 package collections.linkedList;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 public class LinkedList<TValue> {
     private LinkedNode<TValue> first;
     private int size = 0;
@@ -8,8 +12,7 @@ public class LinkedList<TValue> {
         if (first == null)
             first = new LinkedNode(value);
         else {
-            LinkedNode<TValue> tail = first;
-            while (tail.next != null) tail = tail.next;
+            LinkedNode<TValue> tail = find(node -> node.next == null);
             tail.next = new LinkedNode(value);
             tail.next.prev = tail;
         }
@@ -18,52 +21,28 @@ public class LinkedList<TValue> {
     }
 
     public boolean remove(Object value) {
-        for (LinkedNode cur = first;
-             cur != null;
-             cur = cur.next)
-            if (valuesEqual(cur.value, value))
-                return removeNode(cur);
-        return false;
+        return removeNode(find(node -> valuesEqual(node.value, value)));
     }
 
-    private boolean removeNode(LinkedNode<TValue> node) {
-        if (node.prev == null) {
-            first = node.next;
-            if (first != null)
-                first.prev = null;
-        }
-        else
-            node.prev.next = node.next;
-        size--;
-        return true;
+    public TValue removeAt(int index) {
+        LinkedNode<TValue> node = nodeAt(index);
+        removeNode(node);
+        return node.value;
     }
 
-    public TValue elementAt(int index) {
-        return nodeAt(index).value;
-    }
-
-    public int size() {
-        return size;
-    }
-
-    public boolean isEmpty() {
-        return size == 0;
-    }
+    public TValue pop() { return removeAt(0); }
+    public TValue elementAt(int index) { return nodeAt(index).value; }
+    public int size() { return size; }
+    public boolean isEmpty() { return size == 0; }
 
     public boolean contains(Object o) {
-        for (LinkedNode cur = first;
-             cur != null;
-             cur = cur.next)
-            if (valuesEqual(cur.value, o))
-                return true;
-        return false;
+        return find(node -> valuesEqual(node.value, o)) != null;
     }
 
     public Object[] toArray() {
         Object[] list = new Object[size];
-        LinkedNode cur = first;
-        for (int index = 0; cur != null; cur = cur.next)
-            list[index++] = cur.value;
+        AtomicInteger index = new AtomicInteger(0);
+        forEach(node -> list[index.getAndIncrement()] = node.value);
         return list;
     }
 
@@ -78,29 +57,40 @@ public class LinkedList<TValue> {
         return a;
     }
 
-    public TValue removeAt(int index) {
-        LinkedNode<TValue> node = nodeAt(index);
-        removeNode(node);
-        return node.value;
-    }
-
-    public TValue pop() {
-        return removeAt(0);
+    private boolean removeNode(LinkedNode<TValue> node) {
+        if (node == null) return false;
+        if (node.prev == null) {
+            first = node.next;
+            if (first != null)
+                first.prev = null;
+        }
+        else
+            node.prev.next = node.next;
+        size--;
+        return true;
     }
 
     private LinkedNode<TValue> nodeAt(int index) {
-        if (index < 0)
-            throw new IndexOutOfBoundsException();
-        LinkedNode<TValue> node = first;
-        while (index-- > 0 && node != null) node = node.next;
-        if (node == null)
+        AtomicInteger atomicIndex = new AtomicInteger(index);
+        LinkedNode<TValue> node = find(m -> atomicIndex.getAndDecrement() <= 0);
+        if (node == null || index < 0)
             throw new IndexOutOfBoundsException();
         return node;
     }
 
-    private boolean valuesEqual (Object a, Object b) {
-        return a == null &&
-                b == null ||
-                a.equals(b);
+    private LinkedNode<TValue> find(Function<LinkedNode, Boolean> predicate) {
+        for (LinkedNode node = first; node != null; node = node.next)
+            if (predicate.apply(node))
+                return node;
+        return null;
+    }
+
+    private void forEach(Consumer<LinkedNode> action) {
+        for (LinkedNode node = first; node != null; node = node.next)
+            action.accept(node);
+    }
+
+    private boolean valuesEqual(Object a, Object b) {
+        return a == null && b == null || a.equals(b);
     }
 }
