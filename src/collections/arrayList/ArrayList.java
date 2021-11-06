b75df0a19cb391367caa8c81c356af5017af17a4
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-11-06 01:17:01 -0400

    Implement Collection

--- src/collections/arrayList/ArrayList.java
+++ src/collections/arrayList/ArrayList.java
@@ -1,16 +1,107 @@
 package collections.arrayList;
 
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.Objects;
 
-public class ArrayList<T> implements Iterable<T> {
+public class ArrayList<T> implements Collection<T> {
 
-    private final T[] values;
+    private T[] values;
+    private int size = 0;
+
+    public ArrayList() {
+        values = allocateArray(1);
+    }
 
     public ArrayList(T[] values) {
-        this.values = values;
+        if (values.length == 0)
+            this.values = allocateArray(1);
+        else {
+            this.values = values;
+            size = values.length;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean contains(Object value) {
+        for (T item : values)
+            if (Objects.equals(item, value))
+                return true;
+        return false;
     }
 
     public Iterator<T> iterator() {
-        return new ArrayIterator<>(values);
+        return new ArrayIterator<>((T[])toArray());
+    }
+
+    public Object[] toArray() {
+        T[] subArray = allocateArray(size);
+        copyTo(subArray);
+        return subArray;
+    }
+
+    public <T1> T1[] toArray(T1[] a) {
+        if (a.length <= size) return (T1[])toArray();
+        copyTo(a);
+        a[size] = null;
+        return a;
+    }
+
+    public boolean add(T value) {
+        if (values.length == size) grow();
+        values[size++] = value;
+        return true;
+    }
+
+    public boolean remove(Object value) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean containsAll(Collection<?> items) {
+        for (Object item : items)
+            if (!contains(item))
+                return false;
+        return true;
+    }
+
+    public boolean addAll(Collection<? extends T> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean removeAll(Collection<?> items) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void clear() {
+        size = 0;
+    }
+
+    private void grow() {
+        T[] oldList = values;
+        values = allocateArray(values.length * 2);
+        copyTo(oldList, values);
+    }
+
+    private T[] allocateArray(int length) {
+        return (T[]) new Object[length];
+    }
+
+    private void copyTo(Object[] destination) {
+        System.arraycopy(values, 0, destination, 0, size);
+    }
+
+    private void copyTo(T[] source, T[] destination) {
+        System.arraycopy(source, 0, destination, 0, source.length);
     }
 }
--- src/collections/arrayList/ArrayListTest.java
+++ src/collections/arrayList/ArrayListTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Iterator;
+import java.util.Vector;
 
 public class ArrayListTest {
 
@@ -12,8 +13,12 @@ public class ArrayListTest {
         ArrayList<Integer> list = new ArrayList<>(new Integer[0]);
         Iterator<Integer> iterator = list.iterator();
         Assert.assertFalse(iterator.hasNext());
+        Assert.assertEquals(0, list.size());
+        Assert.assertTrue(list.isEmpty());
 
         list = new ArrayList<>(new Integer[]{1, 2, 3});
+        Assert.assertFalse(list.isEmpty());
+        Assert.assertEquals(3, list.size());
         iterator = list.iterator();
         Assert.assertTrue(iterator.hasNext());
         Assert.assertEquals(1, (int)iterator.next());
@@ -22,5 +27,120 @@ public class ArrayListTest {
         Assert.assertTrue(iterator.hasNext());
         Assert.assertEquals(3, (int)iterator.next());
         Assert.assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void addsItemsToEmptyCollection() {
+        ArrayList<Integer> list = new ArrayList<>();
+        Assert.assertTrue(list.isEmpty());
+        Assert.assertTrue(list.add(1));
+        Assert.assertFalse(list.isEmpty());
+        Assert.assertTrue(list.add(2));
+        Assert.assertFalse(list.isEmpty());
+        Assert.assertTrue(list.add(3));
+        Assert.assertFalse(list.isEmpty());
+        Iterator<Integer> iterator = list.iterator();
+        Assert.assertEquals(1, (int)iterator.next());
+        Assert.assertEquals(2, (int)iterator.next());
+        Assert.assertEquals(3, (int)iterator.next());
+        Assert.assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void toArrayReturnsOnlyElementsAddedToList() {
+        ArrayList<Integer> list = new ArrayList<>();
+        Assert.assertArrayEquals(new Integer[0], list.toArray());
+        list.add(1);
+        Assert.assertArrayEquals(new Integer[]{1}, list.toArray());
+        list.add(2);
+        Assert.assertArrayEquals(new Integer[]{1, 2}, list.toArray());
+        list.add(3);
+        Assert.assertArrayEquals(new Integer[]{1, 2, 3}, list.toArray());
+    }
+
+    @Test
+    public void clearsListOfEveryElement() {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.clear();
+        Assert.assertArrayEquals(new Integer[0], list.toArray());
+        Assert.assertEquals(0, list.size());
+        list.add(1);
+        list.clear();
+        Assert.assertArrayEquals(new Integer[0], list.toArray());
+        Assert.assertEquals(0, list.size());
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.clear();
+        Assert.assertArrayEquals(new Integer[0], list.toArray());
+        Assert.assertEquals(0, list.size());
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void removeThrowsUnsupported() {
+        ArrayList<Integer> list = new ArrayList<>();
+        Boolean __ = list.remove(1);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void removeAllThrowsUnsupported() {
+        ArrayList<Integer> list = new ArrayList<>();
+        Boolean __ = list.removeAll(new Vector<Integer>());
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void addAllThrowsUnsupported() {
+        ArrayList<Integer> list = new ArrayList<>();
+        Boolean __ = list.addAll(new Vector<>());
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void retainAllThrowsUnsupported() {
+        ArrayList<Integer> list = new ArrayList<>();
+        Boolean __ = list.retainAll(new Vector<Integer>());
+    }
+
+    @Test
+    public void searchesListForValue() {
+        ArrayList<Number> list = new ArrayList<>();
+        Assert.assertFalse(list.contains(1));
+        list.add(1);
+        Assert.assertTrue(list.contains(1));
+        list.add(2);
+        Assert.assertTrue(list.contains(2));
+    }
+
+    @Test
+    public void checksIfListContainsSublistOfItems() {
+        ArrayList<Integer> list = new ArrayList<>();
+        Vector<Integer> vector = new Vector<>();
+        Assert.assertTrue(list.containsAll(vector));
+        vector.add(1);
+        Assert.assertFalse(list.containsAll(vector));
+        list.add(1);
+        Assert.assertTrue(list.containsAll(vector));
+        list.add(2);
+        Assert.assertTrue(list.containsAll(vector));
+        vector.add(2);
+        Assert.assertTrue(list.containsAll(vector));
+        vector.add(3);
+        list.add(3);
+        Assert.assertTrue(list.containsAll(vector));
+        vector.add(4);
+        Assert.assertFalse(list.containsAll(vector));
+        list.add(4);
+        list.add(4);
+        Assert.assertTrue(list.containsAll(vector));
+    }
+
+    @Test
+    public void copiesElementsToArray() {
+        ArrayList<Integer> list = new ArrayList<>();
+        Assert.assertArrayEquals(new Number[0], list.toArray(new Number[0]));
+        list.add(1);
+        Assert.assertArrayEquals(new Number[] {1}, list.toArray(new Number[0]));
+        Assert.assertArrayEquals(new Number[] {1}, list.toArray(new Number[] {2}));
+        Assert.assertArrayEquals(new Number[] {1, null}, list.toArray(new Number[] {2, 3}));
+        Assert.assertArrayEquals(new Number[] {1, null, 4, 5}, list.toArray(new Number[] {2, 3, 4, 5}));
     }
 }
